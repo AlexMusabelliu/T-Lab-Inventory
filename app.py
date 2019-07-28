0d34178cda46ec75b8dd3239e4f0ae9bae6b8e3f
Pranav Addepalli <pranav.addepalli@gmail.com>
2019-07-28 12:28:32 -0400

    fixed dependency issues

--- app.py
+++ app.py
@@ -4,22 +4,23 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Ashburn@20148'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ProblemTRT?'
 app.config['MYSQL_DATABASE_DB'] = 'inventory'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 connection = mysql.connect()
 cursor = connection.cursor()
 
+
 @app.route('/query', methods=['GET', 'POST'])
 def query():
     return render_template('query.html')
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         tool = request.form['tool']
-        print(tool)
         box_number = request.form['box_number']
         quantity = request.form['quantity']
         quantity_type = request.form['quantity_type']
@@ -33,7 +34,7 @@ def add():
         link_to_video = request.form['link_to_video']
         link_to_acquisition_form = request.form['link_to_acquisition_form']
 
-        cursor.execute("INSERT INTO stuff (name, box_number,year_of_acquisition, quantity, quantity_type,cost,owner,manufacturer_link, equipment_supply, link_to_image, link_to_video, link_to_acquisition_form, course ) VALUES ( 'tool', '1','2019','10','dozens','400','LAB','manufacturer.link.com','EQUIPMENT','image.link.com','video.link.com','acquisitionform.link.com','ENG' );")
+        cursor.execute("INSERT INTO tools(name, box_number, year_of_acquisition, quantity, quantity_type, cost, owner, manufacturer_link, equipment_supply, link_to_image, link_to_video, link_to_acquisition_form, course) VALUES({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})".format(tool, box_number, year_of_acquisition, quantity, quantity_type, cost, owner, manufacturer_link, equipment_supply, link_to_image, link_to_video, link_to_acquisition_form, course))
         connection.commit()
 
     return render_template('add.html')
@@ -41,18 +42,18 @@ def add():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    setup()
     return render_template('home.html')
 
 
-@app.route('/setup', methods=['GET', 'POST'])
 def setup():
     print('setting')
-    cursor.execute("SHOW TABLES LIKE 'stuff'")
+    cursor.execute("SHOW TABLES LIKE 'tools'")
     result = cursor.fetchone()
     print(result)
     if result is None:
         print('making table')
-        cursor.execute("CREATE TABLE stuff (id INT unsigned NOT NULL AUTO_INCREMENT, name VARCHAR(150) NOT NULL, box_number VARCHAR(150) NOT NULL, year_of_acquisition VARCHAR(150) NOT NULL, quantity VARCHAR(150) NOT NULL, quantity_type VARCHAR(150) NOT NULL, cost VARCHAR(150) NOT NULL,  owner VARCHAR (150) NOT NULL, manufacturer_link VARCHAR(500) NOT NULL, equipment_supply VARCHAR(150) NOT NULL, link_to_image VARCHAR(500) NOT NULL, link_to_video VARCHAR(500) NOT NULL, link_to_acquisition_form VARCHAR(500) NOT NULL, course VARCHAR(150) NOT NULL, PRIMARY KEY (id) );")
+        cursor.execute("CREATE TABLE tools (id INT unsigned NOT NULL AUTO_INCREMENT, name VARCHAR(150) NOT NULL, box_number VARCHAR(150) NOT NULL, year_of_acquisition VARCHAR(150) NOT NULL, quantity VARCHAR(150) NOT NULL, quantity_type VARCHAR(150) NOT NULL, cost VARCHAR(150) NOT NULL,  owner VARCHAR (150) NOT NULL, manufacturer_link VARCHAR(500) NOT NULL, equipment_supply VARCHAR(150) NOT NULL, link_to_image VARCHAR(500) NOT NULL, link_to_video VARCHAR(500) NOT NULL, link_to_acquisition_form VARCHAR(500) NOT NULL, course VARCHAR(150) NOT NULL, PRIMARY KEY (id) );")
         connection.commit()
     return redirect('/', code=302)
 
--- setup.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE inventory;
-USE inventory;
